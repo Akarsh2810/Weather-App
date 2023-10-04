fe86a979c81d9083885add5867f35202dcbde058
Akarsh Anand <akarshanand28@gmail.com>
2023-10-04 13:42:23 +0530

    project setup

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  // console.log("Hello World! You are inside Weather App");
   return (
-    <View style={styles.container}>
-      <Text>Hello! You are inside Weather App.</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <Text>Hello! You are inside Weather App.</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <View style = {{height: 100, width: 100, backgroundColor: 'orange'}}>
+      <Text>Hello World</Text>
     </View>
   );
 }
