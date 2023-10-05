3a59b0f1d449c5d5d4dda6f228a56276b2700682
Akarsh Anand <akarshanand28@gmail.com>
2023-10-05 17:17:12 +0530

    implemented loading using ActivityIndicator

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
-import Counter from "./src/demonstrations/Counter";
 
 const App = () => {
+
+  const [loading, setLoading] = useState(true);
+  const {container} = styles;
+  if(loading) {
+    return (
+      <View style = {container}>
+        <ActivityIndicator size={"large"} color={"blue"}/>
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
-      {/* <Counter /> */}
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  }
+})
 
 export default App;
